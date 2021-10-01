a46e32360321175acadbd6f7ce812001296c1a92
oriyonay <oriyonay12@gmail.com>
2021-10-01 14:45:54 -0500

    integrated announcements page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :announcements
+  root 'announcements#index' # temporarily set the announcements page as home, for convenience
+
+  resources :announcements do
+    member do
+      get :delete
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
