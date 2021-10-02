e994a5da50a58cf1086fd0b6010e9d279727f2c7
root <root@localhost>
2021-10-02 17:03:34 +0000

    made template member page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'member/ManageMembers'
   root 'announcements#index' # temporarily set the announcements page as home, for convenience
 
   resources :announcements do
