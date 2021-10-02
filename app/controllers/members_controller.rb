ec880a977908f3d5028dcfee1bb0c296dd097971
andreww-han <68350961+andreww-han@users.noreply.github.com>
2021-10-02 13:48:21 -0500

    Update members_controller.rb
    Repurposed the announcements_controller but for members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,29 +1,77 @@
 class MembersController < ApplicationController
     def index
-        @members = Member.all
+      @members = Member.order('created_at DESC')
+  
+      @is_admin = true # just for testing
+
+      if @is_admin
+        @announcement = Announcement.new
       end
-    
-      def show
-        @member = Member.find(params[:id])
-      end
-    
-      def new
-        @member = Member.new
-      end
-    
-      def create
-        @member = Member.new(member_params)
-    
-        if @member.save
-          redirect_to @member
-        else
-          render :new, status: :unprocessable_entity
-        end
-      end
-    
-      private
-        def member_params
-          params.require(:member).permit(:name)
-        end
+      
     end
-end
+  
+    def new
+      @member = Member.new
+    end
+  
+    def create
+      @member = Member.new(member_params)
+  
+      # we make sure that all fields have been filled in order to prevent any errors later on:
+      unless verify(@member)
+        @error = 'make sure to fill all fields!'
+        render('new')
+        return
+      end
+  
+      if @member.save
+        @flash_notice = 'Member added successfully.'
+        redirect_to(members_path, notice: @flash_notice)
+      else
+        render('index')
+      end
+    end
+  
+    def show
+      @member = Member.find(params[:id])
+    end
+  
+    def edit
+      @member = Member.find(params[:id])
+    end
+  
+    def update
+      @member = Member.find(params[:id])
+      if @member.update(member_params)
+        @flash_notice = 'Member updated successfully.'
+        redirect_to(members_path, notice: @flash_notice)
+      else
+        @error = true
+        render('edit')
+      end
+    end
+  
+    def delete
+      @member = Member.find(params[:id])
+    end
+  
+    def destroy
+      @member = Member.find(params[:id])
+      @member.destroy
+      @flash_notice = 'Member deleted successfully.'
+      redirect_to(members_path, notice: @flash_notice)
+    end
+  
+    private def member_params
+      params.require(:member).permit(:title, :message)
+    end
+  
+    def verify(member)
+      if member.title.empty? or member.message.empty?
+        return false
+      else
+        return true
+      end
+    end
+  end
+  
