064a99e67ecac620e23fa67d7f6049aac99325fe
tylach <tylach@tamu.edu>
2021-10-04 19:27:59 -0500

    added google oauth implementation on main

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  root 'announcements#index' # temporarily set the announcements page as home, for convenience
-
+  resources :logins
+  #root to: 'dashboards#show'
+  #will automatically redirect users to the sign in page upon entering the website
+  root to: 'announcements#index'
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end  # temporarily set the announcements page as home, for convenience
+  # the google oauth tutorial had me set a different root idk how were going to do this
   resources :announcements do
     member do
       get :delete
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_145920) do
+ActiveRecord::Schema.define(version: 2021_10_03_204119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
+
   create_table "announcements", force: :cascade do |t|
     t.string "title"
     t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "logins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
