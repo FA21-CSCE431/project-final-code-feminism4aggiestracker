1e7592667eaad732cb0f836048c084395c3ee970
andreww-han <68350961+andreww-han@users.noreply.github.com>
2021-10-13 15:01:24 -0500

    Revert "uploaded to github classroom"
    This reverts commit f1170b734506d1fadd735f5a85914ab6d731091b.

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_182457) do
+
+ActiveRecord::Schema.define(version: 2021_10_04_231929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "announcements", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+
+  create_table "joins", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "meeting_id", null: false
+    t.bigint "member_id", null: false
+    t.index ["meeting_id"], name: "index_joins_on_meeting_id"
+    t.index ["member_id"], name: "index_joins_on_member_id"
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.boolean "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.date "date"
   end
 
   create_table "members", force: :cascade do |t|
@@ -31,4 +60,6 @@ ActiveRecord::Schema.define(version: 2021_10_02_182457) do
     t.integer "totalPoints", default: 0
   end
 
+  add_foreign_key "joins", "meetings"
+  add_foreign_key "joins", "members"
 end
