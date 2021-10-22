67b7b4fdbdb626245e261f4691bc39e3f4dcec62
natekrall <natekrall@users.noreply.github.com>
2021-10-22 11:07:48 -0500

    deleted date from post and updated crud with cassidy, need to integrate oauth with posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_182238) do
+ActiveRecord::Schema.define(version: 2021_10_22_151224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2021_10_18_182238) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "member_id", null: false
