b4c37f74968f263dca7cfcd4299f12afa87a5f6f
andreww-han <68350961+andreww-han@users.noreply.github.com>
2021-10-23 20:45:42 -0500

    added navigation links

--- db/schema.rb
+++ db/schema.rb
@@ -32,14 +32,6 @@ ActiveRecord::Schema.define(version: 2021_10_23_184158) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.date "date"
-    t.boolean "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "joins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
